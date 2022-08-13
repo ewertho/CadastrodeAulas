8b126fc5955d2e775c7787d4a5d2e53582717c6a
Ewerton Barbosa <ewerton.barbosa.prestserv@petrobras.com.br>
2022-08-13 11:25:41 -0300

    eliminando try catch

--- src/controllers/SubjectControllers.ts
+++ src/controllers/SubjectControllers.ts
@@ -1,21 +1,18 @@
 import { Request, Response } from "express";
+import { BadRequestError } from "../helpers/api-erros";
 import { subjectRespository } from "../respositories/SubjectRepository";
 
 export class SubjectController {
   async create(req: Request, res: Response) {
     const { name } = req.body;
     if (!name) {
-      return res.status(400).json({ message: "Campo nome é obrigatorio" });
+      throw new BadRequestError("Campo nome é obrigatorio");
     }
-    try {
-      const newSubject = subjectRespository.create({
-        name,
-      });
-      await subjectRespository.save(newSubject);
+    const newSubject = subjectRespository.create({
+      name,
+    });
+    await subjectRespository.save(newSubject);
 
-      return res.status(201).json(newSubject);
-    } catch (error) {
-      return res.status(500).json({ message: "Internal server error" });
-    }
+    return res.status(201).json(newSubject);
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { AppDataSource } from "./data-source";
+import { errorMiddleware } from "./middleware/error";
 import routes from "./routes";
 
 AppDataSource.initialize().then(() => {
@@ -8,6 +9,6 @@ AppDataSource.initialize().then(() => {
   app.use(express.json());
 
   app.use(routes);
-
+  app.use(errorMiddleware);
   return app.listen(process.env.PORT);
 });

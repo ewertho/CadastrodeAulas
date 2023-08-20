186ef80baa4d246b53f3fa5d26e28c53e4ccc6fb
ewerton <ewerton.lfb@gmai.com>
2023-08-20 11:19:56 -0300

    melhorias

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { AppDataSource } from "./data-source";
 import { errorMiddleware } from "./middleware/error";
 import routes from "./routes";
 const swaggerUi = require("swagger-ui-express");
-const swaggerFile = require("./swagger_output.json");
+const swaggerFile = require("./../swagger.json");
 
 AppDataSource.initialize().then(() => {
   const app = express();
@@ -13,5 +13,7 @@ AppDataSource.initialize().then(() => {
   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
   app.use("/v1", routes);
   app.use(errorMiddleware);
-  return app.listen(process.env.PORT);
+  return app.listen(process.env.PORT, () => {
+    console.log(`Backend is running on port ${process.env.PORT}`);
+  });
 });

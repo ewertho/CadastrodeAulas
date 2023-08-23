83ae42d12b288773160dd374106e4d88f39356f0
ewerton <ewerton.lfb@gmai.com>
2023-08-23 17:56:26 -0300

    somente uri

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,18 +2,9 @@ import "dotenv/config";
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 
-const port = process.env.DB_PORT as number | undefined;
-
 export const AppDataSource = new DataSource({
   type: "postgres",
   url: process.env.DB_URI,
-  // type: "sqlite",
-  host: process.env.DB_HOST,
-  port: port,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  // database: "./database/database.sqlite",
   entities: [`${__dirname}/**/entities/*.{ts,js}`],
   migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
 });

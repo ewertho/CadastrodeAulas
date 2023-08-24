950a6b8ad3573257c67e4fb4d91b04f6705e2a3f
ewerton <ewerton.lfb@gmai.com>
2023-08-24 10:36:04 -0300

    alteracoes para swagger

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,21 @@ const swaggerFile = require("./../swagger.json");
 
 AppDataSource.initialize().then(() => {
   const app = express();
-
   app.use(express.json());
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+  var options = {
+    customSiteTitle: "Cadastro de Aulas",
+    customfavIcon: "https://avatars.githubusercontent.com/u/6936373?s=200&v=4",
+    customJs: [
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js",
+    ],
+    customCssUrl: [
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css",
+    ],
+  };
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile, options));
   app.use("/v1", routes);
   app.use(errorMiddleware);
   return app.listen(process.env.PORT, () => {

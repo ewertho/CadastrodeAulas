028a84eef47f94c6d1f16aeced4a3a509d3d0e9e
ewerton <ewerton.lfb@gmai.com>
2023-08-24 12:52:01 -0300

    tentativa css 6

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,17 @@ import "express-async-errors";
 import { AppDataSource } from "./data-source";
 import { errorMiddleware } from "./middleware/error";
 import routes from "./routes";
-const swaggerUi = require("swagger-ui-express");
-const swaggerFile = require("./../swagger.json");
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./../swagger.json";
 
 AppDataSource.initialize().then(() => {
   const app = express();
   app.use(express.json());
-  app.use(express.static(__dirname + "/public"));
+  // app.use(express.static(__dirname + "/public"));
   var options = {
     customSiteTitle: "Cadastro de Aulas",
-    customCssUrl: "/assets/styles/custom.css",
+    customCssUrl:
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css",
   };
   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile, options));
   app.use("/v1", routes);

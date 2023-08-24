307b5f9fe65aaf637c1f6f15b0f41e95edc68819
ewerton <ewerton.lfb@gmai.com>
2023-08-24 11:48:37 -0300

    tentativa css 3

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,18 @@ import "express-async-errors";
 import { AppDataSource } from "./data-source";
 import { errorMiddleware } from "./middleware/error";
 import routes from "./routes";
+import "swagger-ui-themes/themes/3.x/theme-flattop.css";
 const swaggerUi = require("swagger-ui-express");
 const swaggerFile = require("./../swagger.json");
 
 AppDataSource.initialize().then(() => {
   const app = express();
   app.use(express.json());
-  var options = {
-    customSiteTitle: "Cadastro de Aulas",
-    customCss: ".swagger-ui .topbar { color:blue, backgroundcolor:red }",
-  };
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile, options));
+  // var options = {
+  //   customSiteTitle: "Cadastro de Aulas",
+  //   customCss: ".swagger-ui .topbar { color:#FFFF; backgroundcolor:#0000 }",
+  // };
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
   app.use("/v1", routes);
   app.use(errorMiddleware);
   return app.listen(process.env.PORT, () => {
